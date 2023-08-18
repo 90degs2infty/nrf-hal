bf5373bce96db11014a2610ebf10b65a67272cda
90degs2infty <90degs2infty@posteo.org>
2023-08-18 14:59:47 +0200

    Add module level documentation to `rtime`

--- nrf52840-hal/src/rtime/mod.rs
+++ nrf52840-hal/src/rtime/mod.rs
@@ -1,3 +1,9 @@
+//! A hal-level interface to the timer peripheral.
+//!
+//! Note that this module kind of by-passes [`nrf52480_hal`'s `timer` module](https://docs.rs/nrf52840-hal/latest/nrf52840_hal/timer/index.html)
+//!
+//! See [Nordic's docs](https://infocenter.nordicsemi.com/topic/ps_nrf52840/timer.html?cp=5_0_0_5_29) for a general overview of the underlying hardware.
+
 pub mod bitmode;
 pub mod interrupt;
 pub mod mode;
